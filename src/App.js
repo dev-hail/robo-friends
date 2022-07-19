e779128871c48780ee4848ea5ce7ee16814036f4
hail <106756914+dev-hail@users.noreply.github.com>
2022-07-19 12:00:26 +0530

    Update hooks

--- src/App.js
+++ src/App.js
@@ -5,22 +5,9 @@ import Scroll from './Scroll.js'
 import './App.css'
 
 function App() {
-// 	constructor() {
-// 	super()
-// 	this.state = {
-// 	robots: [],
-// 	searchfield: ''
-// }
-// 	}
 
 	const  [robots, setRobots] = useState([])
 	const [searchfield, setSearchfield] = useState("")
-
-	// componentDidMount() {
-	// 	fetch('https://jsonplaceholder.typicode.com/users')
-	// 	.then(response=> response.json())
-	// 	.then(users=> this.setState({robots:users}))
-	// }
 
 	useEffect(() => {
 		fetch('https://jsonplaceholder.typicode.com/users')
